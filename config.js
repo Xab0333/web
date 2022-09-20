d2f5bfaeabddd6b64f3177df310a8465ed20b7fb
Xabier <xabing5@gmailc.com>
2022-09-20 15:33:20 +0200

    modified:   config.js 	modified:   guardar.js 	new file:   "imagenes/monta\303\261as.jpg" 	modified:   index.html 	new file:   module.js 	new file:   "monta\303\261as.jpg" 	modified:   styles.css 	new file:   subir.html

--- config.js
+++ config.js
@@ -2,16 +2,21 @@
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
-firebase.initializeApp({
-    apiKey: "AIzaSyBXQ55L3w6vU8ULVGEKvSQPa2ln_vvDQ64",
+var firebaseConfig = {
+  apiKey: "AIzaSyBXQ55L3w6vU8ULVGEKvSQPa2ln_vvDQ64",
   authDomain: "data-4f223.firebaseapp.com",
   projectId: "data-4f223",
   storageBucket: "data-4f223.appspot.com",
   messagingSenderId: "495284296572",
-  appId: "1:495284296572:web:7f39c4295b4bc3f93ba2f9"
-});
+  appId: "1:495284296572:web:7f39c4295b4bc3f93ba2f9",
+  storageBucket: "gs://data-4f223.appspot.com"
+};
 
 
 
+
+firebase.initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 var db = firebase.firestore();
+// Initialize Cloud Firestore and get a reference to the service
+const storage = firebase.storage();
