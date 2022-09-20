4cc19ea0c51462ab619939dbf71b8c518fe31798
Xabier <xabing5@gmailc.com>
2022-09-20 19:48:47 +0200

    modified:   guardar.js 	modified:   index.html

--- guardar.js
+++ guardar.js
@@ -12,63 +12,36 @@ function guardar(){
         alert("Error en el registro");
     });
 }
-function processSelectedFiles(fileInput) {
-    console.log(fileInput);
-    console.log(fileInput.files);
-    var files = fileInput.files;
-    for (var i = 0; i < files.length; i++) {
-           var finalFiles =  (files[i].name);
-      }
-    var storageRef = firebase.storage().ref();
+window.onload = function() {
+    
 
+    // asociamos el manejador de eventos sobre el INPUT FILE
+    document.getElementById('campoarchivo').addEventListener('change', function(evento){
+      evento.preventDefault();
+      var archivo  = evento.target.files[0];
+      subirArchivo(archivo);
+    });
+  }
+
+
+function subirArchivo(archivo) {
+   
     var metadata = {
         contentType: 'image/jpg'
       };
-    
 
-  
-  // Upload file and metadata to the object 'images/mountains.jpg'
-  var uploadTask = storageRef.child('images/' + finalFiles).put(finalFiles, metadata);
-  console.log(finalFiles);
-  
-  // Listen for state changes, errors, and completion of the upload.
-  uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-    (snapshot) => {
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log('Upload is paused');
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log('Upload is running');
-          break;
-      }
-    },
-    (error) => {
-      // A full list of error codes is available at
-      // https://firebase.google.com/docs/storage/web/handle-errors
-      switch (error.code) {
-        case 'storage/unauthorized':
-          // User doesn't have permission to access the object
-          break;
-        case 'storage/canceled':
-          // User canceled the upload
-          break;
-  
-        // ...
-  
-        case 'storage/unknown':
-          // Unknown error occurred, inspect error.serverResponse
-          break;
-      }
-    },
-    () => {
-      // Upload completed successfully, now we can get the download URL
-      uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-        console.log('File available at', downloadURL);
-      });
+      var refStorage = firebase.storage().ref('micarpeta').child(archivo.name);
+      var uploadTask = refStorage.put(archivo);
+      uploadTask.on('state_changed', null,
+        function(error){
+          console.log('Error al subir el archivo', error);
+        },
+        function(){
+          console.log('Subida completada');
+         
+        }
+      );
     }
-  );
-}
+   
+  
+ 
